a68aa65edebf909a03d469612402afff4e14ae92
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-03-20 23:47:56 +0100

    Update scripts

--- get-notebooks.sh
+++ get-notebooks.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-set -e
-
-. /usr/local/bin/start.sh jupyter notebook $*
-# Download Git
-apt-get update
-apt-get -f install
-apt-get install -y git-core
-
 # Get Notebooks from github repository
 cd /tmp && git clone https://github.com/DistributedSystemsGroup/Algorithmic-Machine-Learning
 cd /tmp/Algorithmic-Machine-Learning/ && mv  -v Notebooks/* /home/jovyan/work
