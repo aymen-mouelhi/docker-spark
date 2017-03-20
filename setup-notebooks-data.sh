c902aeaf091b5e85bc37f86623501c046e921191
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-03-21 00:04:45 +0100

    Update scripts

--- setup-notebooks-data.sh
+++ setup-notebooks-data.sh
@@ -6,7 +6,8 @@ apt-get install -y wget
 # Create folder lastfm
 cd /datasets && mkdir lastfm
 
-# Download data
+# Download User Artist Data
 wget http://www.iro.umontreal.ca/~lisa/datasets/profiledata_06-May-2005.tar.gz && tar xvzf profiledata_06-May-2005.tar.gz && rm profiledata_06-May-2005.tar.gz
 mv profiledata_06-May-2005/* /datasets/lastfm
 rm -r profiledata_06-May-2005
+rm lastfm/README.txt
