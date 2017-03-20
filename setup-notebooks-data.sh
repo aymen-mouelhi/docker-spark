f75f5a8aad844a160cf3cdb66286536ecff40392
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-03-20 23:57:07 +0100

    Update scripts

--- setup-notebooks-data.sh
+++ setup-notebooks-data.sh
@@ -8,3 +8,5 @@ cd /datasets && mkdir lastfm
 
 # Download data
 wget http://www.iro.umontreal.ca/~lisa/datasets/profiledata_06-May-2005.tar.gz && tar xvzf profiledata_06-May-2005.tar.gz && rm profiledata_06-May-2005.tar.gz
+mv profiledata_06-May-2005/* /datasets/lastfm
+rm -r profiledata_06-May-2005
